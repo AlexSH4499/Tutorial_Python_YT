a4a2ff40806b6ccfd88f7161ceb3b2001859bf5a
Ghoul <rodrigueznatalandy@gmail.com>
2020-01-16 18:04:48 -0400

    Anadi el archivo de fundamentos y el script pertinente al primer video de YT

---
+++ fundamentos/variables.py
@@ -0,0 +1,30 @@
+# import os, sys, math
+
+
+def tipos_de_vars(variable):
+    print(f"Valor de variable:{variable}")
+    print(f"Tipo de variable:{type(variable)}")
+    print()
+    return
+
+
+
+if __name__ == "__main__":
+    '''
+    dfufhsdbkfhksahdjjksdnjkn
+    xklcvnkzxncv;,jbzxcv,j bjzxc vjxbc hasdjkfbhk
+    kjsnjkshdjgk'''
+
+    # *skjdfh = 23
+    entero = 11
+    flotante = 7.8
+    nombre = "Pikachu"#dfloasiudfuaisdhbfuihasdldfbivaibdflbiadsfibsdlifjbidfbj
+
+
+    eric ='14'#n es la cantidad de caracteres que tengo dentro
+    # tipos_de_vars(variable=entero)
+    # tipos_de_vars(variable=flotante)
+    # tipos_de_vars(variable=nombre)
+    # print(nombre)
+    # print(type(nombre))
+    tipos_de_vars(variable= entero)
